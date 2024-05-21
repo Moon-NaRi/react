61aabf313d7b2259c30c89920babdaae224ca9ab
YJ Kim <gallagherismine@gmail.com>
2024-05-21 23:02:35 +0900

    20230521
    crm server

--- react-crm-project/client/src/App.js
+++ react-crm-project/client/src/App.js
@@ -7,36 +7,19 @@ import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableRow from "@mui/material/TableRow";
-
-const customers = [
-  {
-    "id": 1,
-    "image": 'https://placeimg.com/64/64/1',
-    "name": '문나리',
-    "birthday": '981028',
-    "gender": '여자',
-    "job": '학생',
-  },
-  {
-    "id": 2,
-    "image": 'https://placeimg.com/64/64/2',
-    "name": '김유진',
-    "birthday": '981028',
-    "gender": '여자',
-    "job": '백수',
-  },
-  {
-    "id": 3,
-    "image": 'https://placeimg.com/64/64/3',
-    "name": '박샐리',
-    "birthday": '981028',
-    "gender": '여자',
-    "job": '직장인',
-  }
-]
+import React, {useState,useEffect} from 'react';
 
 //https://mui.com/material-ui/react-table/ 가면 여러가지 샘플 참고 가능
 function App() {
+  
+  const [customers, setCustomers] = useState('');
+
+  useEffect (() => {
+    callApi().then(
+      res => {setCustomers(res);}
+    ).catch(err => console.log(err));
+  }); //mount, update 모두 호출
+
   return (
     <Paper>
     <Table>
@@ -47,15 +30,21 @@ function App() {
         </TableRow>
       </TableHead>
       <TableBody>
-      {
+      { customers ?
         customers.map((customer) => {
           return <Customer customer={customer}/>;
-        })
+        }) : "Now loading...."
       }
       </TableBody>
     </Table>
     </Paper>
   );
+}
+
+async function callApi() {  //비동기호출
+  const response = await fetch('/api/customers'); //접속 API
+  const body = await response.json();
+  return body;
 }
 
 export default App;
--- react-crm-project/server.js
+++ react-crm-project/server.js
@@ -1,13 +1,40 @@
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const mariaDB = require('mariadb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            "id": 1,
+            "image": 'https://placeimg.com/64/64/1',
+            "name": '문나리',
+            "birthday": '981028',
+            "gender": '여자',
+            "job": '학생',
+          },
+          {
+            "id": 2,
+            "image": 'https://placeimg.com/64/64/2',
+            "name": '김유진',
+            "birthday": '981028',
+            "gender": '여자',
+            "job": '백수',
+          },
+          {
+            "id": 3,
+            "image": 'https://placeimg.com/64/64/3',
+            "name": '박샐리',
+            "birthday": '981028',
+            "gender": '여자',
+            "job": '직장인',
+          }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
